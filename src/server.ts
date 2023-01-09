0592b9c1b9740b72ade8ee2f7089990fa364a9b1
Iman Heidari <iman.heydari@gmail.com>
2023-01-08 21:50:23 -0800

    fix the port

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import payload from 'payload';
+import express from "express";
+import payload from "payload";
 
-require('dotenv').config();
+require("dotenv").config();
 const app = express();
 
 // Redirect root to Admin panel
-app.get('/', (_, res) => {
-  res.redirect('/admin');
+app.get("/", (_, res) => {
+  res.redirect("/admin");
 });
 
 // Initialize Payload
@@ -15,10 +15,10 @@ payload.init({
   mongoURL: process.env.MONGODB_URI,
   express: app,
   onInit: () => {
-    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
   },
-})
+});
 
 // Add your own express routes here
 
-app.listen(3000);
+app.listen(process.env.PORT || 80);
